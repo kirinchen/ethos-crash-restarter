3a8433f51b2a5b44d40c369233fd07c4588d3f87
kirin <kirin.chen1001@gmail.com>
2019-06-30 19:10:33 +0800

    change logic

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ build/libs/gs-scheduling-tasks-0.1.0.jar
 
 ### You Need to change
 
-java -DfixedRate=5000 -DfilePath=/STS/Text.txt -jar build/libs/gs-scheduling-tasks-0.1.0.jar
+java -DfixedRate=5000 -DfilePath=/STS/Text.txt -DinitialDelay=1000 -DminFileCount=5 -jar build/libs/gs-scheduling-tasks-0.1.0.jar
  
 * fixedRate = {check time / millisecond} 
 * filePath = {the miner log file / you can cmd show log to watch log path }
--- src/main/java/net/surfm/ethos/ScheduledTasks.java
+++ src/main/java/net/surfm/ethos/ScheduledTasks.java
@@ -1,11 +1,13 @@
 package net.surfm.ethos;
+
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -17,55 +19,53 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledTasks {
 
-    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-    @Value("${fixedRate}")
-    private  int fixedRate = 0;
-    @Value("${filePath}")
-    private  String filePath ;
-    
-    private boolean isWindows = System.getProperty("os.name")
-    		  .toLowerCase().startsWith("windows");
-    
-    private String lastMd5;
-    
+	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	@Value("${fixedRate}")
+	private int fixedRate = 0;
+	@Value("${initialDelay}")
+	private int initialDelay = 0;
+	@Value("${filePath}")
+	private String filePath;
+	@Value("${minFileCount}")
+	private long minFileCount;
 
-    
-    
-    @Scheduled(fixedRateString ="${fixedRate}", initialDelay=1000)
-    public void reportCurrentTime() throws IOException, InterruptedException {
-    	
-    	if(StringUtils.isBlank(filePath)) throw new NullPointerException("not get file path");
-    	
-    	try (InputStream is = Files.newInputStream(Paths.get(filePath))) {
-    	    String md5 = org.apache.commons.codec.digest.DigestUtils.md5Hex(is);
-    	    log.info(" md5 {}", md5);
-    	    if(StringUtils.isBlank(lastMd5)) {
-    	    	lastMd5 = md5;
-    	    }else if(StringUtils.equals(md5, lastMd5)) {
-    	    	callRestart();
-    	    }
-    	}
-    	
-        log.info(fixedRate+" The time is now {}", dateFormat.format(new Date()));
-    }
+	private boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
 
+	private long lastCount;
 
+	@Scheduled(fixedRateString = "${fixedRate}", initialDelayString = "${initialDelay}")
+	public void reportCurrentTime() throws IOException, InterruptedException {
 
+		if (StringUtils.isBlank(filePath))
+			throw new NullPointerException("not get file path");
+
+		try (Stream<String> lines = Files.lines(Paths.get(filePath), Charset.defaultCharset())) {
+
+			long numOfLines = lines.count();
+			log.info(" numOfLines {}", numOfLines);
+			if (lastCount <= minFileCount) {
+				lastCount = numOfLines;
+				log.info(" init Line {} current line {}",minFileCount, numOfLines);
+			} else if (lastCount == numOfLines) {
+				callRestart();
+			}
+		}
+
+		log.info(fixedRate + " The time is now {}", dateFormat.format(new Date()));
+	}
 
 	private void callRestart() throws IOException, InterruptedException {
-		
+
 		String homeDirectory = System.getProperty("user.home");
 		Process process;
 		if (isWindows) {
-		    process = Runtime.getRuntime()
-		      .exec(String.format("cmd.exe /c dir %s", homeDirectory));
+			process = Runtime.getRuntime().exec(String.format("cmd.exe /c dir %s", homeDirectory));
 		} else {
-		    process =Runtime.getRuntime().exec("sudo r");
+			process = Runtime.getRuntime().exec("sudo r");
 		}
-		StreamGobbler streamGobbler = 
-				  new StreamGobbler(process.getInputStream(), System.out::println);
-				Executors.newSingleThreadExecutor().submit(streamGobbler);
+		StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+		Executors.newSingleThreadExecutor().submit(streamGobbler);
 		int exitCode = process.waitFor();
 		log.info(" callRestart {}", exitCode);
 		System.exit(0);
